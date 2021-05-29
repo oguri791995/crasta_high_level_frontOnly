185554ece74a202f956fefcb825f978c43a7fd5d
oguri <okmcjkun50@gmail.com>
2021-05-30 00:35:53 +0900

    ver1.1 about画面

---
+++ JS/about.js
@@ -0,0 +1,9 @@
+toggle()
+
+function toggle(){
+  $('.header-humberger,.nav-item').on("click",function(){
+    $('.humberger').toggleClass('clicked');
+    $("body").toggleClass("noScroll");
+    $(".header-nav").toggleClass("displayNone");
+  });
+}
